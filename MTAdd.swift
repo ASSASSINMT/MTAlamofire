356ce8fb46d999836909208b19a37c0b4c5fe839
matao <449636697@qq.com>
2024-03-25 12:38:43 +0800

    my first time use dev pod make changes to local git pod

---
+++ MTAdd.swift
@@ -0,0 +1,14 @@
+//
+//  MTAdd.swift
+//  Alamofire
+//
+//  Created by tao ma on 2024/3/25.
+//
+
+import Foundation
+
+public class MTAdd {
+    public static func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
