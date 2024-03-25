39ce3571f63f16578176365210836d484b45a9bb
matao <449636697@qq.com>
2024-03-25 12:42:11 +0800

    MT PRINT CLASS

---
+++ MTPrint.swift
@@ -0,0 +1,14 @@
+//
+//  MTPrint.swift
+//  Alamofire
+//
+//  Created by tao ma on 2024/3/25.
+//
+
+import Foundation
+
+public class MTPrint {
+    public static func mt_print(msg: String) {
+        print(msg)
+    }
+}
